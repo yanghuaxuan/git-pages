0f10d159fcfbf129b9fb232344902276c749bc1c
yanghuauxan <yanghuaxuan@gmail.com>
2023-08-23 19:12:52 -0700

    Refactor to use guards

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
 use actix_web::{
     web, App, HttpResponse, HttpServer,
-    Responder, get, post, HttpRequest, http::header::{HeaderName}
+    Responder, HttpRequest, guard::{self, Guard}
 };
 use regex::Regex;
 
-#[get("/")]
-async fn index(req: HttpRequest) -> impl Responder {
-    let server_err = "Internal Server Error \r\n";
-    let bad_req = "Please pass in a valid Host header! \r\n";
+static SERVER_ERR: &str = "Internal Server Error \r\n";
 
+struct HostPatternGuard {
+    host_pattern: Regex
+}
+
+impl Guard for HostPatternGuard {
+    fn check(&self, ctx: &guard::GuardContext<'_>) -> bool {
+        let host: Option<&str> = ctx.head().headers()
+            .get("Host")
+            .map_or(None, |val| val.to_str().ok());
+        let host = match host {
+            Some(val) => val,
+            None => {
+                return false
+            }
+        };
+
+        match self.host_pattern.captures_iter(host).next() {
+            Some(_) => true,
+            None => false
+        }
+    }
+}
+
+#[allow(non_snake_case)]
+fn HostPattern(host_pattern: Regex) -> HostPatternGuard {
+    HostPatternGuard { host_pattern }
+}
+
+async fn try_pages(req: HttpRequest) -> impl Responder {
     let root_domain = std::env::var("ROOT_DOMAIN").expect("Environmental variabble ROOT_DOMAIN must be defined!");
     let re_domain = Regex::new(&format!(r#"((?P<username>\w*)\.)?((?P<repo>\w*)\.)?{}"#, root_domain)).unwrap();
 
-    let file = match std::fs::read_to_string("./templates/index.html") {
-        Ok(val) => val,
-        Err(_) => {
-            return HttpResponse::InternalServerError().body(server_err)
-        }
-    };
-
-    let host: Option<&str> = req.headers()
+    // Valid Host header should already be validated by Guards
+    let host = req.headers()
         .get("Host")
-        .map_or(None, |val| val.to_str().ok());
-    let host = match host {
-        Some(val) => val,
-        None => {
-            return HttpResponse::BadRequest().body(bad_req)
-        }
-    };
+        .unwrap()
+        .to_str()
+        .unwrap();
 
     let dom_caps = re_domain.captures(&host);
 
@@ -36,13 +52,18 @@ async fn index(req: HttpRequest) -> impl Responder {
             let username = &dom_caps.name("username").map_or("", |m| m.as_str());
             let repo = &dom_caps.name("repo").map_or("", |m| m.as_str());
 
-            if username.len() == 0 && repo.len() == 0 {
-                return HttpResponse::Ok().body(file)
-            }
-
             HttpResponse::Ok().body(format!("username: {username}\nrepo: {repo}\n"))
         },
         None => HttpResponse::InternalServerError().into()
+    }
+}
+
+async fn index() -> impl Responder {
+    match std::fs::read_to_string("./templates/index.html") {
+        Ok(val) => HttpResponse::Ok().body(val),
+        Err(_) => {
+            return HttpResponse::InternalServerError().body(SERVER_ERR)
+        }
     }
 }
 
@@ -55,9 +76,22 @@ async fn not_found() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     println!("Starting server...");
 
+    let root_domain = std::env::var("ROOT_DOMAIN").expect("Environmental variabble ROOT_DOMAIN must be defined!");
+    let re_domain = Regex::new(&format!(r#"((?P<username>\w*)\.)?((?P<repo>\w*)\.){}"#, root_domain)).unwrap();
+
     HttpServer::new(move || {
         App::new()
-        .service(index)
+        .service(
+            web::scope("/")
+            .guard(guard::Host(format!("{root_domain}")))
+            .route("", web::to(index)))
+        .service(
+            web::scope("/")
+            .guard(HostPattern(re_domain.to_owned()))
+            .route("", web::to(try_pages)))
+        .service(
+            web::scope("/")
+            .route("", web::to(|| async { HttpResponse::BadRequest()} )))
         .default_service(
             web::route().to(not_found)
         )
